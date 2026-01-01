42524b145ec5e833212ee089d1bc6f969e8949b3
vivianmartinez <martinezpvivi@gmail.com>
2026-01-01 14:22:13 +0100

    Modify the Task model to use Factory, create TaskFactory, run seeder

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'user_id'
+    ];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Crear 5 usuarios 
+        User::factory(5)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Crear 30 tareas asignadas a usuarios aleatorios
+        Task::factory(30)->create();
     }
 }
